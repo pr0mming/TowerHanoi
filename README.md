340c9f06887f3d61dc1e8163bfb990d70d675f19
pr0mming <samjdk007@gmail.com>
2019-04-19 20:46:55 -0500

    First Commit
    :)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# TowerHanoi
-Tower of Hanoi in JavaScript using Phaser Framework
+![hanoi](https://user-images.githubusercontent.com/20020612/56449768-aeb97a00-62e3-11e9-9b1e-0f4f5bf1da2e.png)
+
+# Tower of Hanoi-JS
+This is a small simulator of the towers of Hanoi developed in JavaScript using <a href="http://phaser.io/">Phaser.js (v2.6.1)</a>, it fits up to 8 discs although you can customize it by changing the speed of game
+
+# Demo
+Watch the demo in this video: https://www.youtube.com/watch?v=4g_4nM0yOCE
---
+++ src/Stopwatch.js
@@ -0,0 +1,81 @@
+// This code was not written by me!
+// Source code: https://codepen.io/_Billy_Brown/pen/dbJeh
+
+class Stopwatch {
+    constructor(display) {
+        this.running = false;
+        this.display = display;
+        this.laps = [];
+        this.reset();
+        this.print(this.times);
+    }
+    
+    reset() {
+        this.times = [ 0, 0, 0 ];
+    }
+    
+    start() {
+        if (!this.time) this.time = performance.now();
+        if (!this.running) {
+            this.running = true;
+            requestAnimationFrame(this.step.bind(this));
+        }
+    }
+    
+    stop() {
+        this.running = false;
+        this.time = null;
+    }
+
+    restart() {
+        if (!this.time) this.time = performance.now();
+        if (!this.running) {
+            this.running = true;
+            requestAnimationFrame(this.step.bind(this));
+        }
+        this.reset();
+    }
+    
+    clear() {
+        //clearChildren(this.results);
+    }
+    
+    step(timestamp) {
+        if (!this.running) return;
+        this.calculate(timestamp);
+        this.time = timestamp;
+        this.print();
+        requestAnimationFrame(this.step.bind(this));
+    }
+    
+    calculate(timestamp) {
+        var diff = timestamp - this.time;
+        // Hundredths of a second are 100 ms
+        this.times[2] += diff / 10;
+        // Seconds are 100 hundredths of a second
+        if (this.times[2] >= 100) {
+            this.times[1] += 1;
+            this.times[2] -= 100;
+        }
+        // Minutes are 60 seconds
+        if (this.times[1] >= 60) {
+            this.times[0] += 1;
+            this.times[1] -= 60;
+        }
+    }
+    
+    print() {
+        this.display.setText('TIME: ' + this.format(this.times));
+    }
+    
+    format(times) {
+        return this.pad0(times[0], 2) + ":" + this.pad0(times[1], 2) + ":" + this.pad0(Math.floor(times[2]), 2);
+    }
+    
+    pad0(value, count) {
+        var result = value.toString();
+        for (; result.length < count; --count)
+            result = '0' + result;
+        return result;
+    }
+}
